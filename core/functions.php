27f12352ec221754ef8dd50a305522e3d57007cc
Jay Bayron <jaybayron400@gmail.com>
2023-05-15 18:42:32 +0800

    Update functions.php
    remove die dump function to ready for deplyoyment

--- core/functions.php
+++ core/functions.php
@@ -1,12 +1,12 @@
 <?php 
 
-function dd($value) {
-    echo '<pre>';
-        var_dump($value);
-    echo '</pre>';
+// function dd($value) {
+//    echo '<pre>';
+//        var_dump($value);
+//    echo '</pre>';
     
-    die();
-}
+//    die();
+// }
 
 function urlIs($value) {
     return parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY) === $value;
